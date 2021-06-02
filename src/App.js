bdceb5a0d6f16a14f683e6e4efc6d6d1cfc1712e
Davis Jordan <davisridleyjordan@gmail.com>
2021-06-02 16:13:14 -0400

    Site header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import headshot from './images/headshot.png'
+
+import * as Icon from "react-feather"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={headshot} className="HeaderImage"/>
         <p className="HeaderText">
-          Edit <code>src/App.js</code> and save to reload.
+          Davis Jordan
+          <br />
+          <a href="https://www.linkedin.com/in/davis-j/">
+            <Icon.Linkedin className="HeaderIcon"/>
+          </a>
+          &nbsp;
+          &nbsp;
+          <a href="https://www.github.com/dav-jordan">
+            <Icon.GitHub className="HeaderIcon" />
+          </a>
+          &nbsp;
+          &nbsp;
+          <a href="mailto:davisridleyjordan@gmail.com">
+            <Icon.Mail className="HeaderIcon" />
+          </a>
         </p>
       </header>
     </div>
